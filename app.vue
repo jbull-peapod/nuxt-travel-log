472d8a7e5b853ef316c9da7275df401045902c14
jbull-peapod <john.bull@peapod.com>
2025-06-20 10:29:02 -0700

    adds landing page

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-    <h1 class="underline font-bold text-2xl">
-      Hello, Nuxt Travel Log!
-    </h1>
-    <button class="btn btn-active btn-secondary">View Map
-      <Icon name="tabler:map"/>
-    </button>
-</div></template>
+<NuxtLayout>
+  <NuxtPage />
+</NuxtLayout>  
+</template>
